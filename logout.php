f8a97dbf24437278e9b9533f92c5dac0c01ac7cb
Timur <ocendolgaamuzyka@gmail.com>
2025-10-10 14:44:33 +0300

    Добавил защиту от перехода по ссылке на logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,30 @@
 <?php 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
   require_once 'config.php';
   unset($_SESSION['user']); // удаляем хеш из сессии
   $redirect = $_SESSION['redirect_after_login'] ?? 'index.php';
   header('Location: ' . $redirect);
+} else {
+  $redirect = $_SESSION['redirect_after_login'] ?? 'index.php';
 
+  header('Location: ' . $redirect);
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Вы уверены?</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <form>
+    <button>Yes</button>
+    <button>No</button>
+   </form> 
+</body>
+</html>
