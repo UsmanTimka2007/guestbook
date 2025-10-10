beecd08e144dcca7a20c96ffb5c7b4a48954972d
Timur <ocendolgaamuzyka@gmail.com>
2025-10-10 13:40:19 +0300

    Открыл config.php и перенёс пароли в отдельный файл

--- config.php
+++ config.php
@@ -2,10 +2,6 @@
 
 session_start();
 
-$host = 'localhost';
-$db   = 'guestbook_db';
-$user = 'root';
-$pass = '123qwe!@#';
 
 try {
 	$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
